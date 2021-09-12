f4e20da9ce38c99143c7967cdddebba1ed03a7d5
Nick <67605183+SteamingSoup@users.noreply.github.com>
2021-09-11 19:42:44 -0500

    Update server.py
    Condensed the code and fixed some errors

--- server/server.py
+++ server/server.py
@@ -1,64 +1,46 @@
 import socket
+from socket import AF_INET, SOCK_STREAM
 from threading import Thread
 
-SERVER_HOST = "0.0.0.0"
-SERVER_PORT = 6677
-BUFSIZ = 1024
-separator_token = "<SEP>"
+server_host = "0.0.0.0"
+server_port = 6677
+input_buffer = 1024
+addr = (server_host, server_port)
+server = socket(AF_INET, SOCK_STREAM)
+server.bind(addr)
 
-client_sockets = set()
-
-TCP_socket = socket.socket()
-
-TCP_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-TCP_socket.bind((SERVER_HOST, SERVER_PORT))
-
-TCP_socket.listen(5)
-
-print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
-
-clients = {}
+client_sockets = {}
 addresses = {}
 
-
-def incoming_connections(cs):
+def incoming_connections():
     while True:
-        try:
-            msg = cs.recv(1024).decode
-        except Exception as expt:
-            print(f"***Error***: {expt}")
-            client_sockets.remove(cs)
-
-    while True:
-        client_socket, client_address = TCP_socket.accept()
+        client, client_address = server.accept()
         print(f"{client_address} has connected.")
-        client_socket.sent(bytes("You are now connected.", "utf8"))
-        client_sockets.add(client_socket)
-        addresses[client_socket] = client_address
+        client.send(bytes("You are now connected.", "utf8"))
+        addresses[client] = client_address
         Thread(target=handle_client, args=(client_socket,)).daemon = True
         Thread(target=handle_client, args=(client_socket,)).start()
 
 
-def handle_client(client_socket):
-    name = client_socket.recv(BUFSIZ).decode("utf8")
+def handle_client(client):
+    name = client.recv(input_buffer).decode("utf8")
     rules = f"Welcome {name}, please type {quit} if you need to exit."
-    client_socket.send(bytes(rules, "utf8"))
+    client.send(bytes(rules, "utf8"))
     join = f"{name} has joined the chat"
     broadcast(bytes(join, "utf8"))
-    client_sockets[client_socket] = name
+    clients[client] = name
 
     while True:
-        msg = client_socket.recv(BUFSIZ)
+        msg = client.recv(input_buffer)
         if msg != bytes("{quit}", "{utf8}"):
             broadcast(msg, name + ": ")
         else:
-            client_socket.send(bytes("{quit}", "utf8"))
-            client_socket.close()
-            del client_sockets[client_socket]
+            client.send(bytes("{quit}", "utf8"))
+            client.close()
+            del clients[client]
             broadcast(bytes(f"{name} has left the chat.", "utf8"))
             break
 
 def broadcast(msg, prefix=""):
-    for sock in client_sockets:
-        sock.send(bytes(prefix, "utf8")+msg)
+    for sockt in clients:
+        sockt.send(bytes(prefix, "utf8")+msg)
